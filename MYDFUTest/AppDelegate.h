c967c4682d710d92e5beb50563ca3e1b208d04b5
pjl <1970668107@qq.com>
2017-03-02 09:36:16 +0800

    Initial Commit

---
+++ MYDFUTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYDFUTest
+//
+//  Created by 彭江玲 on 2017/3/2.
+//  Copyright © 2017年 shanren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
